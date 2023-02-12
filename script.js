f805b311d5d9568eb713112cd033900db19252a7
yungzeeongit <greyw4300@gmail.com>
2023-02-12 14:31:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 /*-- constants  --*/
+const winningCombo = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[1,4,7],
+[2,5,8],
+[0,3,6],
+[0,4,8],
+[2,4,6]
+]
 
 /*-- app's states(variables)  --*/
 let board
 let turn = 'X'
+let win
 
 /*-- cached elements references  --*/
 const squares = Array.from(document.querySelectorAll('#board div'))
-
+const messages = document.querySelector('h2')
 
 
 /*-- event listeners --*/
@@ -25,6 +36,7 @@ function initGame() {
 function render() {
     board.forEach(function(mark,index){
 squares[index].textContent = mark    });
+messages.textContent = win?` ${win} wins this round`:`Its ${turn}'s turn`
 
 }
 
@@ -33,10 +45,21 @@ function handleTurn(event) {
         return square === event.target
     })
     board[idx] = turn
+win = getWinner()
 
 turn = turn === "X"?"O":"X"
 render()
 
+}
+
+function getWinner() {
+    let winner = null
+
+    winningCombo.forEach(function(combo,index){
+        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]])
+        winner = board[combo[0]]
+    })
+    return winner
 }
 
 
