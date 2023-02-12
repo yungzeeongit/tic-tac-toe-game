9cebb5826dee159a2f31755d3a9ec7d1b325c3ef
yungzeeongit <greyw4300@gmail.com>
2023-02-11 16:59:11 -0800

    ssss

---
+++ script.js
@@ -0,0 +1,49 @@
+/*-- constants  --*/
+
+/*-- app's states(variables)  --*/
+let board
+let turn = 'X'
+
+/*-- cached elements references  --*/
+const squares = Array.from(document.querySelectorAll('#board div'))
+
+
+
+/*-- event listeners --*/
+document.getElementById('board').addEventListener('click',handleTurn)
+
+
+/*-- functions --*/
+
+function initGame() {
+    board = ["2", "5", "6",
+            "2", "3", "5"
+          , "8", "6", "7"]
+        render()
+}
+
+function render() {
+    board.forEach((mark,index) => {
+squares[index].textContent = mark    });
+
+}
+
+function handleTurn(event) {
+    let idx = squares.findIndex(function (square) {
+        return square = event.target
+    })
+    board[idx] = turn
+    console.log(board)
+ }
+
+
+
+
+
+
+
+
+
+ initGame();
+
+
